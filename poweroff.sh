0487b67e1a7a04473ccc0d6fbb8f203a2038189e
pulo0 <bartexx13042005@gmail.com>
2023-04-29 18:26:05 +0200

    fixed the code and typos and added an option to cancel in 1 minute option

--- poweroff.sh
+++ poweroff.sh
@@ -3,36 +3,60 @@
 date
 chmod 755 poweroff.sh
 echo "Location of a script - $PWD"
+
 options=("Power Off - Shuts down the device immediately" 
-"Power Off in 10 seconds - Shuts down the device in 10 seconds" 
+"Power Off in 1 minute - Shuts down the device in 1 minute" 
 "Reboot - Reboot the device" 
 "Log Off - logs of from the root" 
 "Quit")
+
+quitOptions=("Yes" "No")
 
 echo "Choose an option of exiting a device"
 select opt in "${options[@]}"
 do
 case $opt in
 "Power Off - Shuts down the device immediately")
-    echo "Shutting down a device"
-    systemctl poweroff
+    echo "Shutting down a device..."
+    systemctl shutdown now
     ;;
-"Power Off in 10 seconds - Shuts down the device in 10 seconds")
-    echo "Shutting down a device in 10 second"
-    systemctl poweroff
+"Power Off in 1 minute - Shuts down the device in 1 minute")
+    echo "Shutting down a device in 10 second..."
+    systemctl shutdown -t 1
     ;;
 "Reboot - Reboot the device")
     echo "Rebooting the device..."
     reboot
     ;;
 "Log Off - logs of from the root")
-    echo "Logginf off the root..."
-    logout
+    echo "Logging off the root..."
+    exit
     ;;
 "Quit")
-    break;
+    break
     ;;
 *)
-    echo "invalid option $REPLY";;
+    echo "Invalid option '$REPLY'";;
     esac
 done
+
+if [[$opt == "Power Off in 10 seconds - Shuts down the device in 10 seconds"]]
+then
+    echo "Do you want to cancel the shutdown?"
+    select quit in "${quitOptions[@]}"
+    do
+    case $quit in
+    "Yes")
+        echo "Cancelling..."
+        systemctl shutdown -c
+        ;;
+    "No")
+        echo "See you on the other side :)"
+        break
+        ;;
+    *)
+        echo "Invalid option '$REPLY'";;
+        esac
+    done
+else
+    break
