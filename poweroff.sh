f8dbf885ba8e0d6b90e0bca1b0780d4fc08be97b
pulo0 <bartexx13042005@gmail.com>
2023-04-29 17:10:00 +0200

    changed pretty much everything

--- poweroff.sh
+++ poweroff.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
 
+date
 chmod 755 poweroff.sh
-echo "You're located in : $PWD"
+echo "Location of a script - $PWD"
+options=("Power Off - Shuts down the device immediately" 
+"Power Off in 10 seconds - Shuts down the device in 10 seconds" 
+"Reboot - Reboot the device" 
+"Log Off - logs of from the root" 
+"Quit")
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "it's linux"
+echo "Choose an option of exiting a device"
+select opt in "${options[@]}"
+do
+case $opt in
+"Power Off - Shuts down the device immediately")
+    echo "Shutting down a device"
     systemctl poweroff
-elif [[ "$OSTYPE" == "msys"* ]]; then
-    echo "it's windows"
-    shutdown -s
-elif [[ "$OSTYPE" == "cygwin"* ]]; then
-    echo "it's windows"
-    shutdown -s
-fi
+    ;;
+"Power Off in 10 seconds - Shuts down the device in 10 seconds")
+    echo "Shutting down a device in 10 second"
+    systemctl poweroff
+    ;;
+"Reboot - Reboot the device")
+    echo "Rebooting the device..."
+    reboot
+    ;;
+"Log Off - logs of from the root")
+    echo "Logginf off the root..."
+    logout
+    ;;
+"Quit")
+    break;
+    ;;
+*)
+    echo "invalid option $REPLY";;
+    esac
+done
